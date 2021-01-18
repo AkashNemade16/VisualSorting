dd65327cea193b6330bf786a35e0b5363fe9c458
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-01-18 13:46:09 -0800

    buildHeap

--- src/Algorithms/heapSort.js
+++ src/Algorithms/heapSort.js
@@ -1,20 +1,35 @@
-import { SwipeableDrawer } from "@material-ui/core";
 
 function heapSort(array){
 
 }
 
 function buildHeap(array){
-   let i = 0;
-   let parent = [i/2];
-   let leftChild = [2*i];
-   let rightChild = [2*i+1];
-   let endIdx = array.length;
-   let temp = []
+    let i = 0;
+    let leftChild = [2*i];
+    let rightChild = [2*i+1];
    
-   for(i=0;i<endIdx;i++){
-       if(array.length===1){
-        temp.push(array[i]);
-       }
+    let parent = [i/2];
+  
+   for(let i=1;i<array.length;i++){
+    
+    if(array[leftChild]>array[parent]){
+        swap(leftChild,parent,array);
+    }
+    else if(array[rightChild]>array[parent]){
+        swap(rightChild,parent,array);
+    }else{
+        swap(leftChild,parent,array);
+    }
+
    }
+   
+   
 }
+
+function swap(a,b,array){
+let temp = array[a];
+ array[a]  = array[b];
+ array[b] = temp;
+}
+
+console.log(buildHeap([10,20,15,30,40]));
