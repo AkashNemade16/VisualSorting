5ef661ad67bbea1988fe723b98fc76e846233aeb
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-01-18 15:27:42 -0800

    heap sort

--- src/Algorithms/heapSort.js
+++ src/Algorithms/heapSort.js
@@ -1,35 +1,60 @@
+let MaxHeap = function() {
+	
+	let heap = [null];
+	
+	this.print = () => heap;
 
-function heapSort(array){
+	this.insert = function(num) {
+		heap.push(num);
+		if (heap.length > 2) {
+			let idx = heap.length - 1;
+			while (heap[idx] > heap[Math.floor(idx/2)]) {
+				if (idx >= 1) {
+					[heap[Math.floor(idx/2)], heap[idx]] = [heap[idx], heap[Math.floor(idx/2)]];
+					if (Math.floor(idx/2) > 1) {
+						idx = Math.floor(idx/2);
+					} else {
+						break;
+					};
+				};
+			};
+		};
+	};
+	
+	this.remove = function() {
+		let smallest = heap[1];
+		if (heap.length > 2) {
+			heap[1] = heap[heap.length - 1];
+			heap.splice(heap.length - 1);
+			if (heap.length === 3) {
+				if (heap[1] < heap[2]) {
+					[heap[1], heap[2]] = [heap[2], heap[1]];
+				};
+				return smallest;
+			};
+			let i = 1;
+			let left = 2 * i;
+			let right = 2 * i + 1;
+			while (heap[i] <= heap[left] || heap[i] <= heap[right]) {
+				if (heap[left] > heap[right]) {
+					[heap[i], heap[left]] = [heap[left], heap[i]];
+					i = 2 * i
+				} else {
+					[heap[i], heap[right]] = [heap[right], heap[i]];
+					i = 2 * i + 1;
+				};
+				left = 2 * i;
+				right = 2 * i + 1;
+				if (heap[left] === undefined || heap[right] === undefined) {
+					break;
+				};
+			};
+		} else if (heap.length === 2) {
+			heap.splice(1, 1);
+		} else {
+			return null;
+		};
+		return smallest;
+	};
 
-}
-
-function buildHeap(array){
-    let i = 0;
-    let leftChild = [2*i];
-    let rightChild = [2*i+1];
-   
-    let parent = [i/2];
-  
-   for(let i=1;i<array.length;i++){
-    
-    if(array[leftChild]>array[parent]){
-        swap(leftChild,parent,array);
-    }
-    else if(array[rightChild]>array[parent]){
-        swap(rightChild,parent,array);
-    }else{
-        swap(leftChild,parent,array);
-    }
-
-   }
-   
-   
-}
-
-function swap(a,b,array){
-let temp = array[a];
- array[a]  = array[b];
- array[b] = temp;
-}
-
-console.log(buildHeap([10,20,15,30,40]));
+};
