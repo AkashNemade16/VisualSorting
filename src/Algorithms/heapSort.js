e5919301365c990adbe06bb65813629331777e0f
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-01-24 22:18:15 -0800

    heapSort

--- src/Algorithms/heapSort.js
+++ src/Algorithms/heapSort.js
@@ -1,46 +1,82 @@
-var arrayLength;
-function heapSort(array){
-    buildHeap(array);
+const buildMaxHeap = (arr) => {
+   // Get index of the middle element
+   let i = Math.floor(arr.length / 2 - 1);
 
-    for(let i=array.length-1;i>0;i++){
-        swap(array,0,i);
-        arrayLength --;
-        heapify(array,0);
-    }return array;
+   // Build a max heap out of
+   // All array elements passed in
+   while (i >= 0) {
+      heapify(arr, i, arr.length);
+      i -= 1;
+   }
 }
 
-function buildHeap(array){
-    arrayLength = array.length;
+const heapify = (heap, i, max) => {
+   let index;//largest
+   let leftChild;
+   let rightChild;
 
-    for(let i = Math.floor(arrayLength/2);i>=0;i++){
-        heapify(array,i);
-    }
+   while (i < max) {
+      index = i;
+
+      // Get the left child index 
+      // Using the known formula
+      leftChild = 2 * i + 1;
+
+      // Get the right child index 
+      // Using the known formula
+      rightChild = leftChild + 1;
+
+      // If the left child is not last element 
+      // And its value is bigger
+      if (leftChild < max && heap[leftChild] > heap[index]) {
+         index = leftChild;
+      }
+
+      // If the right child is not last element 
+      // And its value is bigger
+      if (rightChild < max && heap[rightChild] > heap[index]) {
+         index = rightChild;
+      }
+
+      // If none of the above conditions is true
+      // Just return
+      if (index === i) {
+         return;
+      }
+
+      // Else swap elements
+      swap(heap, i, index);
+
+      // Continue by using the swapped index
+      i = index;
+   }
 }
 
-function heapify(array,i){
-    
-    let leftChild = 2*i+1;
-    let rightChild = 2*i+2;
-    let max = i;
-    
-    if(leftChild<arrayLength && array[leftChild]>array[max]){
-        max = leftChild;
-    }
-    if(rightChild<arrayLength && array[rightChild]>array[max]){
-        max = rightChild;
-    }
-    if(max!==i){
-        swap(array,i,max);
-        heapify(array,max);
-    }
-   
+const swap = (arr, firstItemIndex, lastItemIndex) => {
+   const temp = arr[firstItemIndex];
+
+   // Swap first and last items in the array
+   arr[firstItemIndex] = arr[lastItemIndex];
+   arr[lastItemIndex] = temp;
 }
 
-function swap(a,b,array){
-let temp = array[a];
- array[a]  = array[b];
- array[b] = temp;
+const heapSort = (arr) => {
+   // Build max heap
+   buildMaxHeap(arr);
+
+   // Get the index of the last element
+   let lastElement = arr.length - 1;
+
+   // Continue heap sorting until we have
+   // One element left
+   while (lastElement > 0) {
+      swap(arr, 0, lastElement);
+      heapify(arr, 0, lastElement);
+      lastElement -= 1;
+   }
+
+   // Return sorted array
+   return arr;
 }
 
-var example = [10,20,15,30,40];
-console.log(heapSort(example));
+console.log(heapSort([9,6,4,2,5,8,5,3]));
