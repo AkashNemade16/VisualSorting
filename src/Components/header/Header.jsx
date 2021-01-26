13944f0846f25e98c58654ef45e5098491cf1696
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-01-25 22:54:38 -0800

    newheader

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,49 +1,41 @@
 import React from 'react';
-import {
-    withStyles,
-    AppBar,
-    Toolbar,
-    Typography,
-    Button,
+import {makeStyles,
+AppBar,
+Toolbar,
+Typography,
+Button,
+IconButton,
 } from '@material-ui/core';
-import SimpleMenu from '../Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 
-const useStyles = theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    
-  }); 
+export default function Header(){
+  const classes = useStyles();
+  return(
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+         
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
 
-
-  
-   class Header extends React.Component{
-      
-      render(){
-       
-        const {classes} = this.props; 
-          return (
-            <div className={classes.root}>
-            <AppBar position="static" style={{backgroundColor:"grey"}}  >
-            <Toolbar>
-                <Typography variant="h6" className={classes.title}>
-                VisualSorting
-                </Typography>
-                
-               <SimpleMenu/>
-               
-            </Toolbar>
-            </AppBar>
-        </div>
-          )
-          }
-      }
-  
-      export default withStyles(useStyles)(Header);
