2515e989719b833242cd9fb8d4483fe2a48ec156
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-02-02 12:07:07 -0800

    changed to lifecycle method

--- src/visual.jsx
+++ src/visual.jsx
@@ -1,71 +1,57 @@
-import React,{useState,useEffect} from 'react';
+import React,{Component} from 'react';
 import SimpleMenu from './Components/Menu';
 import {Grid,Button,makeStyles, Typography} from '@material-ui/core';
 import Bar from './Components/Charts/Charts';
 
 //should render random numbers even onpress
-export default function Visual(props){
-  // console.log(props);
-  const [array,setArray] = useState();
-  console.log(array);
-  useEffect(()=>{
-   resetArray();
-  },[])
 
-  function resetArray() {
+export default class Visual extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      array:[],
+    };
+  }
+
+  componentDidMount(){
+    this.resetArray(); 
+  }
+
+  
+
+  resetArray() {
     const array = [];
     for (let i = 0; i < 300; i++) {
       array.push(rand(5, 730));
     }
-     setArray({array});
+     this.setState({array});
   }
-  // function ArrayList(props){
-  //   const array= props.array
-  //   const arrayItems= array.maps((array)=>
-  //     <li key={array.toString()}>
-  //     {arrayItems}
-  //   </li>
-  //   );
-  //   return(
-  //     <ul>{arrayItems}</ul>
-  //   )
-  // }
+
   
-  // const returnedArray = Array.from(array);
-  // console.log(returnedArray);
-  return (
-    <div className="visual">
-      
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-      >
-        <SimpleMenu />
-          
-        <Bar data={setArray}/>
-       <>
-       {
+   
 
-       }
-       </>
-            
-         <Button variant="contained" color="primary" onClick={()=>resetArray()}>
-        Reset Array
-      </Button>
+  render() {
+    return (
+      <div>
 
-      </Grid>
-    </div>
-  );
+      </div>
+    )
+  }
+
 }
-
- 
-
-function rand  (min, max)  {
+  
+function rand (min, max)  {
         let randomNum = Math.random() * (max - min) + min;
         return Math.floor(randomNum);
       }
+  
+  // const returnedArray = Array.from(array);
+  // console.log(returnedArray);
+  
+
+ 
+
    
       
      
