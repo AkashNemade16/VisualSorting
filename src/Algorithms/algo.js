6c82b49fcd359ecf1bad9b73c4e1cebbe51eba56
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-02-28 22:24:06 -0800

    mergeSort bug

---
+++ src/Algorithms/algo.js
@@ -0,0 +1,98 @@
+const shuffle = arr => {
+    var i, j, tmp;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    return arr;
+};
+
+const sleep = milliseconds => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+/*
+ *
+ * -------- SORTING ALGORITHMS --------
+ *
+ */
+const bubbleSort = async reference => {
+    let chart = reference.chartInstance;
+    const dataArray = chart.data.datasets[0].data;
+    const meta = chart.getDatasetMeta(0);
+    const colors = chart.data.datasets[0].backgroundColor;
+    const defaltColor = 'rgb(255, 99, 132)';
+    let tmp;
+
+    for (let i = 0; i < dataArray.length; i++) {
+        for (let j = 0; j < dataArray.length - i - 1; j++) {
+            colors[j] = '#991fc4';
+            chart.data.datasets[0].backgroundColor = colors;
+
+            chart.update();
+            await sleep(10);
+
+            if (dataArray[j] > dataArray[j + 1]) {
+                tmp = dataArray[j];
+                dataArray[j] = dataArray[j + 1];
+                dataArray[j + 1] = tmp;
+
+                tmp = meta.data[j];
+                meta.data[j] = meta.data[j + 1];
+                meta.data[j + 1] = tmp;
+
+                colors[j + 1] = '#991fc4';
+            }
+
+            colors[j] = defaltColor;
+            chart.data.datasets[0].backgroundColor = colors;
+        }
+        colors[dataArray.length - i - 1] = '#7cc746';
+        chart.data.datasets[0].backgroundColor = colors;
+        chart.update();
+    }
+};
+
+const mergeSort = async reference => {
+    let chart = reference.chartInstance;
+    const dataArray = chart.data.datasets[0].data;
+    const meta = chart.getDatasetMeta(0);
+    const colors = chart.data.datasets[0].backgroundColor;
+    const defaltColor = 'rgb(255, 99, 132)';
+    let tmp;
+
+    for (let i = 0; i < dataArray.length; i++) {
+        for (let j = 0; j < dataArray.length - i - 1; j++) {
+            colors[j] = '#991fc4';
+            chart.data.datasets[0].backgroundColor = colors;
+
+            chart.update();
+            await sleep(10);
+
+            if (dataArray[j] > dataArray[j + 1]) {
+                tmp = dataArray[j];
+                dataArray[j] = dataArray[j + 1];
+                dataArray[j + 1] = tmp;
+
+                tmp = meta.data[j];
+                meta.data[j] = meta.data[j + 1];
+                meta.data[j + 1] = tmp;
+
+                colors[j + 1] = '#991fc4';
+            }
+
+            colors[j] = defaltColor;
+            chart.data.datasets[0].backgroundColor = colors;
+        }
+        colors[dataArray.length - i - 1] = '#7cc746';
+        chart.data.datasets[0].backgroundColor = colors;
+        chart.update();
+    }
+};
+
+export default {
+    shuffle: shuffle,
+    bubbleSort: bubbleSort
+};
--- src/Components/Charts/Charts.jsx
+++ src/Components/Charts/Charts.jsx
@@ -1,20 +1,109 @@
-import React,{Component} from 'react';
+import React from 'react';
 import {Bar} from 'react-chartjs-2';
-
+import Select from 'react-select';
+import algo from './Algorithms/algo.js';
+import './Chart.css';
 
 //bring array values and id here
+class Bars extends React.Component {
+  state = {
+    array: algo.shuffle([...Array(51).keys()].splice(1)),
+    buttonDisabled: false,
+    selectedAlgorithm: algo.bubbleSort,
+    algorithms: [
+      { name: algo.bubbleSort, label: 'Bubble Sort' },
+      { name: 'strawberry', label: 'Quick Sort' },
+      { name: 'vanilla', label: 'Merge Sort' }
+    ]
+  };
+  arraySorted = false;
 
+  shuffleArray = e => {
+    this.arraySorted = false;
+    const array = [...this.state.array];
+    algo.shuffle(array);
+    this.setState({ array: array, buttonDisabled: false });
+  };
 
-export default class BarChart extends Component {
-    state={
-        
-    }
-    render() {
+  startSorting = reference => {
+    this.setState({ buttonDisabled: true }, () => {
+      this.state.selectedAlgorithm(reference).then(() => {
+        this.arraySorted = true;
+        this.setState({ buttonDisabled: false });
+      });
+    });
+  };
 
-        return (
-            <div>
+  changeAlgorithm = algorithm => {
+    this.setState({ selectedAlgorithm: algorithm.name });
+  };
 
-            </div>    
-        )
-    }
+  render() {
+    const customStyles = {
+      option: provided => ({
+        ...provided,
+        backgroundColor: '#FFFFFF',
+        color: '#000000'
+      })
+    };
+
+    return (
+      <>
+        <Bar
+          data={{
+            labels: this.state.array,
+            datasets: [
+              {
+                data: this.state.array,
+                backgroundColor: [...Array(this.state.array.length).keys()].map(() => {
+                  return this.arraySorted ? '#7cc746' : 'rgb(255, 99, 132)';
+                })
+              }
+            ]
+          }}
+          options={{
+            legend: {
+              display: false
+            },
+            scales: {
+              yAxes: [{ display: false }],
+              xAxes: [{ gridLines: { color: 'rgba(255, 99, 132, 0.2)' } }]
+            },
+            animation: {
+              duration: 80,
+              xAxes: true,
+              yAxes: true
+            },
+            tooltips: {
+              enabled: false
+            }
+          }}
+          ref={reference => (this.reference = reference)}
+          redraw
+        />
+
+        <Select
+          styles={customStyles}
+          className='dropdown'
+          defaultValue={this.state.algorithms[0]}
+          options={this.state.algorithms}
+          onChange={this.changeAlgorithm}
+          isDisabled={this.state.buttonDisabled}
+        />
+
+        <button
+          onClick={() => this.startSorting(this.reference)}
+          disabled={this.state.buttonDisabled}
+        >
+          Sort
+        </button>
+
+        <button onClick={this.shuffleArray} disabled={this.state.buttonDisabled}>
+          Shuffle
+        </button>
+      </>
+    );
+  }
 }
+
+export default Bars;
